2b4c44f89f09532a90cab1dda6eb69c9721e28b1
RulerTek Tracking Systems <info.rulertek@gmail.com>
2026-08-01 02:25:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('RulerTek website loaded');
+ <!-- Schema.org LocalBusiness -->
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "RulerTek Tracking Systems",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Mogappair",
+        "addressRegion": "Chennai, Tamil Nadu",
+        "addressCountry": "IN"
+      },
+      "telephone": "+91 75987 81902",
+      "email": "info.rulertek@gmail.com"
+    }
+    </script>
+
+    <title>RulerTek Tracking Systems | Premium GPS Solutions</title>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
